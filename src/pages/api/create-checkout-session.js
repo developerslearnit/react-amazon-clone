4759f6ca1000c532bd8030b7421ad81fc1accf78
Mark <mark2kk@gmail.com>
2021-11-07 09:39:59 +0100

    Updated source

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')('sk_test_RlbgmTYYETA85U4o4a2qRILa');
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
 export default async (req, res) => {
     try {
